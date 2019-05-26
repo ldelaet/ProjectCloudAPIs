6eef2caf03d4f1b13a077eec9c3f9be7cd3731d7
Lorenzo De Laet <lorenzodl@live.be>
2019-05-26 23:24:44 +0200

    Project beta
    Beta versie van het project met de Pokeapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService, Pokemon, Pokemons } from './data.service';
+import { rendererTypeName } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements OnInit {
+  Pokemons: Pokemons;
+  Pokemon: Pokemon;
+  _searchID: number;
+  _searchName: string;
+  Wrong: boolean = false;
+  Image: string;
+
+  constructor(private dataService: DataService){
+  }
+  getPokemon(){
+    return this.dataService.getPokemon(this._searchID)
+    .subscribe(data => this.Pokemon = data);
+    
+  }
+  getPokemonName(){
+    return this.dataService.getPokemon(this._searchName)
+    .subscribe(data => this.Pokemon = data);
+    
+  }
+ 
+  ngOnInit(){
+  }
+  get SearchID() {
+    return this._searchID;
+  }
+  set SearchID(value: number) {
+    this._searchID = value;
+    if(this._searchID != null && this._searchID != 0 && this._searchID <= 964) {
+      this.getPokemon();
+      
+      this.Wrong = false;
+    }
+    if(this._searchID == null || this._searchID == 0)
+    {this.Wrong = true;}
+
+    
+    }
+
+    get SearchName() {
+      return this._searchName;
+    }
+    set SearchName(value: string) {
+      this._searchName = value;
+      if(this._searchName != null) {
+        this.getPokemonName();
+        
+        this.Wrong = false;
+      }
+      if(this._searchName == null || this._searchName == "")
+      {this.Wrong = true;}
+  
+      
+      }
+    
+  }
+  
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
